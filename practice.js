928e3a381b43f77890578a99e18c721771571e66
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-09 16:04:15 -0500

    Regex validate PIN code

--- practice.js
+++ practice.js
@@ -30,3 +30,23 @@ function getMiddle(s) {
 function filter_list(l) {
     return l.filter((c) => typeof c === "number");
 }
+
+//ATM machines allow 4 or 6 digit PIN codes
+//and PIN codes cannot contain anything
+//but exactly 4 digits or exactly 6 digits.
+
+//If the function is passed a valid PIN string,
+//return true, else return false.
+
+//Examples:
+//"1234"   -->  true
+//"12345"  -->  false
+//"a234"   -->  false
+
+function validatePIN(pin) {
+    if ((pin.length === 4 || pin.length === 6) && Number.isInteger(+pin)) {
+        return true;
+    } else {
+        return false;
+    }
+}
