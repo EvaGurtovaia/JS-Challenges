1c85c56091246c2a668575005b9ba5f6c66ff417
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-14 18:57:25 -0500

    Mexican Wave

--- practice.js
+++ practice.js
@@ -151,3 +151,32 @@ function findUniq(arr) {
         }
     }
 }
+
+//Task
+//In this simple Kata your task is to create
+// a function that turns a string into a Mexican Wave.
+//You will be passed a string and you must return
+//that string in an array where an uppercase letter is a person standing up.
+
+//Rules
+//1.  The input string will always be lower case but maybe empty.
+//2.  If the character in the string is whitespace then pass over it as
+//if it was an empty seat
+
+//Example
+//wave("hello") => ["Hello", "hEllo", "heLlo", "helLo", "hellO"]
+
+function wave(str) {
+    let mexicanWave = [];
+    for (let i = 0; i < str.length; i++) {
+        let letter = str[i];
+        if (str[i] === " ") {
+            continue;
+        } else {
+            mexicanWave.push(
+                str.slice(0, i) + letter.toUpperCase() + str.slice(i + 1)
+            );
+        }
+    }
+    return mexicanWave;
+}
