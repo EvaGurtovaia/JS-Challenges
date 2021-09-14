3e64eecac5743678a19297b8dcd45c9122821657
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-14 16:51:05 -0500

    Find the unique number

--- practice.js
+++ practice.js
@@ -120,3 +120,34 @@ function findOdd(A) {
 }
 
 console.log(findOdd([1, 3, 1, 3, 1]));
+
+function findUniq(arr) {
+    let strayEl = arr[0];
+    if (strayEl !== arr[1] && strayEl !== arr[2]) {
+        return strayEl;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (strayEl !== arr[i]) {
+            return arr[i];
+        }
+    }
+}
+
+//There is an array with some numbers.
+//All numbers are equal except for one. Try to find it!
+
+//findUniq([ 1, 1, 1, 2, 1, 1 ]) === 2
+//findUniq([ 0, 0, 0.55, 0, 0 ]) === 0.55
+//It’s guaranteed that array contains at least 3 numbers.
+
+function findUniq(arr) {
+    let strayEl = arr[0];
+    if (strayEl !== arr[1] && strayEl !== arr[2]) {
+        return strayEl;
+    }
+    for (let i = 0; i < arr.length; i++) {
+        if (strayEl !== arr[i]) {
+            return arr[i];
+        }
+    }
+}
