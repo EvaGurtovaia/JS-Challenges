01955a1a625ae46bbab8d1c472cb8be704d3ca9a
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-20 20:07:31 -0500

    Count characters in your string

--- practice.js
+++ practice.js
@@ -195,3 +195,20 @@ function getCount(str) {
     }
     return vowelsCount;
 }
+
+//The main idea is to count all the occurring characters in a string.
+//If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+//What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(str) {
+    const result = {};
+    for (const char of str) {
+        if (result.hasOwnProperty(char)) {
+            result[char] += 1;
+        } else {
+            result[char] = 1;
+        }
+    }
+    return result;
+}
