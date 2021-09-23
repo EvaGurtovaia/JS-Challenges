66027a87f9caf821f01ac69caf4943806d0805e7
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-23 12:57:15 -0500

    Updated practice_functions

--- practice_functions.js
+++ practice_functions.js
@@ -33,3 +33,33 @@ function isPangram(str) {
 }
 
 console.log(isPangram("the quick brown fox jumps over  the lazy dog"));
+
+function pick(arr) {
+    //return random element for array
+    const idx = Math.floor(Math.random() * arr.length);
+    return arr[idx];
+}
+
+function getCard() {
+    const values = [
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "10",
+        "J",
+        "Q",
+        "K",
+        "A",
+    ];
+
+    const suits = ["clubs", "spades", "hearts", "diamonds"];
+    return {
+        suit: pick(suits),
+        value: pick(values),
+    };
+}
