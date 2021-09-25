63f71cc65c2631604626150a84a2a891ee514a37
EvaGurtovaia <eva.gurtovaya@gmail.com>
2021-09-25 13:59:29 -0500

    Update practice_functions.js

--- practice_functions.js
+++ practice_functions.js
@@ -63,3 +63,22 @@ function getCard() {
         value: pick(values),
     };
 }
+
+function multiplyBy(num) {
+    return function (x) {
+        return x * num;
+    };
+}
+
+const triple = multiplyBy(3);
+
+console.log(triple(5));
+
+function makeBetween(x, y){
+    return function(num) {
+      return num >= x && num <=y;
+    }
+  }
+  
+  const result = makeBetween(0, 18);
+  console.log(result(5));
