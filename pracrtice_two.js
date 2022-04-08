0083e23cce9ff852103fa212282e53012784ad7e
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-08 13:46:13 -0500

    Print A Rectangle Using Asterisks Challenges

--- pracrtice_two.js
+++ pracrtice_two.js
@@ -134,3 +134,20 @@ function finalGrade(g, p) {
     }
     return 0;
 }
+
+//Print a Rectangle Using Asterisks
+
+//Write a method that, given two arguments, width and height, returns a string representing a rectangle with those dimensions.
+//The rectangle should be filled with spaces, and its borders should be composed of asterisks (*).
+//For example, the following call: getRectangleString(3, 3);
+//Should return the following string:
+//***
+//* *
+//***
+
+function getRectangleString(width, height) {
+    const rn = "\r\n";
+    const tb = "*".repeat(width) + rn;
+    const center = (width > 1) ? ("*" + " ".repeat(width-2) + "*" + rn).repeat(height-2) : "";
+    return height > 1 ? (tb + center + tb) : tb.repeat(height);
+  }
