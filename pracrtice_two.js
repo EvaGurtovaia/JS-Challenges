8888314a2c220917528e417d845beec240b9bad7
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-10 12:35:23 -0500

    Convert Number To Reversed Array Of Digits Challenge

--- pracrtice_two.js
+++ pracrtice_two.js
@@ -187,3 +187,15 @@ function findMissing(arr1, arr2) {
         }
     }
 }
+
+//Convert number to reversed array of digits
+//Given a random non-negative number, you have to return the digits of this number within an array in reverse order.
+
+//Example:
+//348597 => [7,9,5,8,4,3]
+//0 => [0]
+
+function digitize(n) {
+    const nums = n.toString().split("").reverse();
+    return nums.map(el => +el);
+  }
