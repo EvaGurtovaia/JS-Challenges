c3a3040fe77a7956cfb6b84ac1a7c2e564e21118
EvaGurtovaia <eva.gurtovaya@gmail.com>
2022-04-10 13:48:28 -0500

    Can We Divide It Challenge

--- pracrtice_two.js
+++ pracrtice_two.js
@@ -210,3 +210,25 @@ function digitize(n) {
 function findDifference(a, b) {
     return Math.abs(a[0] * a[1] * a[2] - b[0] * b[1] * b[2]);
 }
+
+//Can we divide it?
+
+//Your task is to create the functionisDivideBy (or is_divide_by) to check if an integer number is divisible by both integers a and b.
+//A few cases:
+
+//(-12, 2, -6)  ->  true
+//(-12, 2, -5)  ->  false
+
+//(45, 1, 6)    ->  false
+//(45, 5, 15)   ->  true
+
+//(4, 1, 4)     ->  true
+//(15, -5, 3)   ->  true
+
+function isDivideBy(num, a, b) {
+    if (num % a === 0 && num % b === 0) {
+        return true;
+    } else {
+        return false;
+    }
+}
